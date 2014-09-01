5ce1fcf6ec5f97ce03ae1ca6a456f4c6f3e1d586
Umair <unaveed@gmail.com>
2014-09-01 17:34:27 -0600

    Added images and finalized layout

--- app/src/main/java/edu/utah/cs4962/light/LightActivity.java
+++ app/src/main/java/edu/utah/cs4962/light/LightActivity.java
@@ -3,13 +3,19 @@ package edu.utah.cs4962.light;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 
 public class LightActivity extends Activity {
+
+    ImageView _lightImageView;
+    Switch _lightSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,30 @@ public class LightActivity extends Activity {
         LinearLayout rootLayout = new LinearLayout(this);
         rootLayout.setOrientation(LinearLayout.VERTICAL);
 
-        View someView = new View(this);
-        someView.setBackgroundColor(Color.RED);
+        _lightImageView = new ImageView(this);
+        _lightImageView.setImageResource(R.drawable.off);
+//        _lightImageView.setBackgroundColor(Color.RED);
+        rootLayout.addView(_lightImageView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 2));
 
-        Switch lightSwitch = new Switch(this);
+        _lightSwitch = new Switch(this);
+//        _lightSwitch.setBackgroundColor(Color.GREEN);
+        LinearLayout.LayoutParams lightSwitchLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 0, 1);
+        lightSwitchLayoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+        rootLayout.addView(_lightSwitch, lightSwitchLayoutParams);
 
-        setContentView(someView);
+        _lightSwitch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (_lightSwitch.isChecked()) {
+                    _lightImageView.setImageResource(R.drawable.on);
+                }
+                else {
+                    _lightImageView.setImageResource(R.drawable.off);
+                }
+            }
+        });
+
+        setContentView(rootLayout);
     }
 }
